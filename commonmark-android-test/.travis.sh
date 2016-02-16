8c87c0f028796f9c942b7a44a68d4a494f12d9a6
Robin Stocker <rstocker@atlassian.com>
2016-02-16 16:58:48 +1100

    Add android test project to Travis build matrix

---
+++ commonmark-android-test/.travis.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+version=$(cd .. && mvn help:evaluate -Dexpression=project.version | grep -v '^\[' | tail -1)
+autolink_version=$(cd ../commonmark-ext-autolink && mvn help:evaluate -Dexpression=autolink.version | grep -v '^\[' | tail -1)
+
+touch test.properties
+echo "path.report=../report" >> test.properties
+echo "version.maven=0.4.1" >> test.properties
+echo "version.maven_autolink=0.4.0" >> test.properties
+echo "version.snapshot=$version" >> test.properties
+echo "version.snapshot_autolink=$autolink_version" >> test.properties
+
+echo no | android create avd --force -n test -t "android-15"
+emulator -avd test -no-audio -no-window &
+android-wait-for-emulator
+
+TERM=dumb ./gradlew --stacktrace :app:connectedSnapshotDebugAndroidTest
