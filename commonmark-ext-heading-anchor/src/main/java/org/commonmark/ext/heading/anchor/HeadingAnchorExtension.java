1b529ba6490e5ab20249788bcd2e32e9496ef4b3
Robin Stocker <rstocker@atlassian.com>
2016-09-23 14:53:02 +1000

    Fix Javadoc problems

--- commonmark-ext-heading-anchor/src/main/java/org/commonmark/ext/heading/anchor/HeadingAnchorExtension.java
+++ commonmark-ext-heading-anchor/src/main/java/org/commonmark/ext/heading/anchor/HeadingAnchorExtension.java
@@ -2,34 +2,28 @@ package org.commonmark.ext.heading.anchor;
 
 import org.commonmark.Extension;
 import org.commonmark.ext.heading.anchor.internal.HeadingIdAttributeProvider;
-import org.commonmark.renderer.html.HtmlRenderer;
 import org.commonmark.renderer.html.AttributeProvider;
 import org.commonmark.renderer.html.AttributeProviderContext;
 import org.commonmark.renderer.html.AttributeProviderFactory;
+import org.commonmark.renderer.html.HtmlRenderer;
 
 /**
  * Extension for adding auto generated IDs to headings.
  * <p>
  * Create it with {@link #create()} and then configure it on the builder
  * {@link HtmlRenderer.Builder#extensions(Iterable)}).
- * </p>
  * <p>
  * The heading text will be used to create the id. Multiple headings with the
  * same text will result in appending a hyphen and number. For example:
- * </p>
- * <code>
- * <pre>
+ * <pre><code>
  * # Heading
  * # Heading
- * </pre>
- * </code>
+ * </code></pre>
  * will result in
- * <code>
- * <pre>
+ * <pre><code>
  * &lt;h1 id="heading"&gt;Heading&lt;/h1&gt;
  * &lt;h1 id="heading-1"&gt;Heading&lt;/h1&gt;
- * </pre>
- * </code>
+ * </code></pre>
  *
  * @see IdGenerator the IdGenerator class if just the ID generation part is needed
  */
