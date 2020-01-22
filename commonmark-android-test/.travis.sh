6999ad7c0ed1642d4562dc981535ac50eb07a8b0
Robin Stocker <rstocker@atlassian.com>
2020-01-22 11:41:27 +1100

    Bump versions in docs

--- commonmark-android-test/.travis.sh
+++ commonmark-android-test/.travis.sh
@@ -8,7 +8,7 @@ autolink_version=$(cd ../commonmark-ext-autolink && mvn help:evaluate -Dexpressi
 
 touch test.properties
 echo "path.report=../report" >> test.properties
-echo "version.maven=0.13.1" >> test.properties
+echo "version.maven=0.14.0" >> test.properties
 echo "version.maven_autolink=0.10.0" >> test.properties
 echo "version.snapshot=$version" >> test.properties
 echo "version.snapshot_autolink=$autolink_version" >> test.properties
