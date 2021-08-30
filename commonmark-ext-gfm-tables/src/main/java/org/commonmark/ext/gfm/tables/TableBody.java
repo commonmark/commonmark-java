0ca2d51790089e795c86cdd4fb46371fbff91ed6
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-30 17:23:18 +0800

    Delete commonmark-ext-gfm-tables directory

--- commonmark-ext-gfm-tables/src/main/java/org/commonmark/ext/gfm/tables/TableBody.java
+++
@@ -1,9 +0,0 @@
-package org.commonmark.ext.gfm.tables;
-
-import org.commonmark.node.CustomNode;
-
-/**
- * Body part of a {@link TableBlock} containing {@link TableRow TableRows}.
- */
-public class TableBody extends CustomNode {
-}
--- commonmark-ext-gfm-tables/src/main/java/org/commonmark/ext/gfm/tables/TableHead.java
+++
@@ -1,9 +0,0 @@
-package org.commonmark.ext.gfm.tables;
-
-import org.commonmark.node.CustomNode;
-
-/**
- * Head part of a {@link TableBlock} containing {@link TableRow TableRows}.
- */
-public class TableHead extends CustomNode {
-}
