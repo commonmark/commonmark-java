ec66829b7541b2539cc2126b8f4df4414e2607b8
Robin Stocker <robin@nibor.org>
2024-01-16 22:30:52 +1100

    Add markerIndent and contentIndent to ListItem
    This information is required for a renderer producing Markdown.

--- commonmark/src/test/java/org/commonmark/test/Nodes.java
+++ commonmark/src/test/java/org/commonmark/test/Nodes.java
@@ -14,4 +14,27 @@ public class Nodes {
         }
         return children;
     }
+
+    /**
+     * Recursively try to find a node with the given type within the children of the specified node.
+     *
+     * @param parent    The node to get children from (node itself will not be checked)
+     * @param nodeClass The type of node to find
+     */
+    public static <T> T find(Node parent, Class<T> nodeClass) {
+        Node node = parent.getFirstChild();
+        while (node != null) {
+            Node next = node.getNext();
+            if (nodeClass.isInstance(node)) {
+                //noinspection unchecked
+                return (T) node;
+            }
+            T result = find(node, nodeClass);
+            if (result != null) {
+                return result;
+            }
+            node = next;
+        }
+        return null;
+    }
 }
