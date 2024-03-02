414ba56a17b545601b5360d2f7ddeb7fca30b20b
Robin Stocker <robin@nibor.org>
2024-03-02 21:03:01 +1100

    Update Unicode punctuation to include symbols

--- commonmark/src/main/java/org/commonmark/text/Characters.java
+++ commonmark/src/main/java/org/commonmark/text/Characters.java
@@ -57,17 +57,23 @@ public class Characters {
     }
 
     /**
-     * @see <a href="https://spec.commonmark.org/0.29/#punctuation-character">punctuation character</a>
+     * @see <a href="https://spec.commonmark.org/0.31.2/#unicode-punctuation-character">Unicode punctuation character</a>
      */
     public static boolean isPunctuationCodePoint(int codePoint) {
         switch (Character.getType(codePoint)) {
-            case Character.CONNECTOR_PUNCTUATION:
+            // General category "P" (punctuation)
             case Character.DASH_PUNCTUATION:
-            case Character.END_PUNCTUATION:
-            case Character.FINAL_QUOTE_PUNCTUATION:
-            case Character.INITIAL_QUOTE_PUNCTUATION:
-            case Character.OTHER_PUNCTUATION:
             case Character.START_PUNCTUATION:
+            case Character.END_PUNCTUATION:
+            case Character.CONNECTOR_PUNCTUATION:
+            case Character.OTHER_PUNCTUATION:
+            case Character.INITIAL_QUOTE_PUNCTUATION:
+            case Character.FINAL_QUOTE_PUNCTUATION:
+                // General category "S" (symbol)
+            case Character.MATH_SYMBOL:
+            case Character.CURRENCY_SYMBOL:
+            case Character.MODIFIER_SYMBOL:
+            case Character.OTHER_SYMBOL:
                 return true;
             default:
                 switch (codePoint) {
