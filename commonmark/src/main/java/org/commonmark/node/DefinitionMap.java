c68809dacbf739087ec97d39db98ca0cc0fbad0a
Robin Stocker <robin@nibor.org>
2024-07-06 23:15:54 +1000

    Documentation tweaks

--- commonmark/src/main/java/org/commonmark/node/DefinitionMap.java
+++ commonmark/src/main/java/org/commonmark/node/DefinitionMap.java
@@ -5,9 +5,10 @@ import org.commonmark.internal.util.Escaping;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
- * A map that can be used to store and lookup reference definitions by a label. The labels are case-insensitive and
+ * A map that can be used to store and look up reference definitions by a label. The labels are case-insensitive and
  * normalized, the same way as for {@link LinkReferenceDefinition} nodes.
  *
  * @param <D> the type of value
@@ -28,13 +29,16 @@ public class DefinitionMap<D> {
 
     public void addAll(DefinitionMap<D> that) {
         for (var entry : that.definitions.entrySet()) {
+            // Note that keys are already normalized, so we can add them directly
             definitions.putIfAbsent(entry.getKey(), entry.getValue());
         }
     }
 
     /**
-     * Store a new definition unless one is already in the map. If there is no definition for that label, return null.
+     * Store a new definition unless one is already in the map. If there is no definition for that label yet, return null.
      * Otherwise, return the existing definition.
+     * <p>
+     * The label is normalized by the definition map before storing.
      */
     public D putIfAbsent(String label, D definition) {
         String normalizedLabel = Escaping.normalizeLabelContent(label);
@@ -44,13 +48,17 @@ public class DefinitionMap<D> {
     }
 
     /**
-     * Lookup a definition by normalized label.
+     * Look up a definition by label. The label is normalized by the definition map before lookup.
      *
      * @return the value or null
      */
     public D get(String label) {
         String normalizedLabel = Escaping.normalizeLabelContent(label);
         return definitions.get(normalizedLabel);
+    }
+
+    public Set<String> keySet() {
+        return definitions.keySet();
     }
 
     public Collection<D> values() {
