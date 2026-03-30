f8f7684fb67d92d679ff87da4a32e89c4588fdd3
Spirit Wolf <spirit@canva.com>
2026-03-30 19:24:09 +1100

    add node depth limit

--- commonmark/src/test/java/org/commonmark/test/AbstractVisitorTest.java
+++ commonmark/src/test/java/org/commonmark/test/AbstractVisitorTest.java
@@ -3,9 +3,38 @@ package org.commonmark.test;
 import org.commonmark.node.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AbstractVisitorTest {
+    @Test
+    public void maxDepthMustBeZeroOrGreater() {
+        assertThatThrownBy(() -> new RecordingVisitor(-1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void maxDepthZeroVisitsOnlyRoot() {
+        var paragraph = paragraphTree();
+        var visitor = new RecordingVisitor(0);
+
+        paragraph.accept(visitor);
+
+        assertThat(visitor.visited).containsExactly("paragraph");
+    }
+
+    @Test
+    public void maxDepthOneVisitsDirectChildrenButNotGrandchildren() {
+        var paragraph = paragraphTree();
+        var visitor = new RecordingVisitor(1);
+
+        paragraph.accept(visitor);
+
+        assertThat(visitor.visited).containsExactly("paragraph", "emphasis", "text:tail");
+    }
 
     @Test
     public void replacingNodeInVisitorShouldNotDestroyVisitOrder() {
@@ -33,5 +62,40 @@ public class AbstractVisitorTest {
         assertThat(node).isInstanceOf(Code.class);
         Code code = (Code) node;
         assertThat(code.getLiteral()).isEqualTo(expectedLiteral);
+    }
+
+    private static Paragraph paragraphTree() {
+        var paragraph = new Paragraph();
+        var emphasis = new Emphasis();
+        emphasis.appendChild(new Text("nested"));
+        paragraph.appendChild(emphasis);
+        paragraph.appendChild(new Text("tail"));
+        return paragraph;
+    }
+
+    private static final class RecordingVisitor extends AbstractVisitor {
+        private final List<String> visited = new ArrayList<>();
+
+        private RecordingVisitor(int maxDepth) {
+            super(maxDepth);
+        }
+
+        @Override
+        public void visit(Paragraph paragraph) {
+            visited.add("paragraph");
+            super.visit(paragraph);
+        }
+
+        @Override
+        public void visit(Emphasis emphasis) {
+            visited.add("emphasis");
+            super.visit(emphasis);
+        }
+
+        @Override
+        public void visit(Text text) {
+            visited.add("text:" + text.getLiteral());
+            super.visit(text);
+        }
     }
 }
