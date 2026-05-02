d649322b25dd6cbc934cddfff0de7385f82c6857
Ryan DeStefano <67760716+rdestefa@users.noreply.github.com>
2026-05-02 01:54:16 -0700

    Add Support for Custom Alert Titles

--- commonmark-test-util/src/main/java/org/commonmark/testutil/RenderingTestCase.java
+++ commonmark-test-util/src/main/java/org/commonmark/testutil/RenderingTestCase.java
@@ -1,7 +1,5 @@
 package org.commonmark.testutil;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public abstract class RenderingTestCase {
 
     protected abstract String render(String source);
