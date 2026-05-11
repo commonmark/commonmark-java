978e4b089646020328a54e6698c2a267b3222ecc
Stuart Parker <2979257+stupar73@users.noreply.github.com>
2026-05-11 14:13:16 +1000

    Add support for Markdown rendering for GFM task list items

--- commonmark-ext-task-list-items/src/main/java/org/commonmark/ext/task/list/items/TaskListItemsExtension.java
+++ commonmark-ext-task-list-items/src/main/java/org/commonmark/ext/task/list/items/TaskListItemsExtension.java
@@ -1,10 +1,16 @@
 package org.commonmark.ext.task.list.items;
 
+import java.util.Set;
 import org.commonmark.Extension;
 import org.commonmark.ext.task.list.items.internal.TaskListItemHtmlNodeRenderer;
+import org.commonmark.ext.task.list.items.internal.TaskListItemMarkdownNodeRenderer;
 import org.commonmark.ext.task.list.items.internal.TaskListItemPostProcessor;
 import org.commonmark.parser.Parser;
+import org.commonmark.renderer.NodeRenderer;
 import org.commonmark.renderer.html.HtmlRenderer;
+import org.commonmark.renderer.markdown.MarkdownNodeRendererContext;
+import org.commonmark.renderer.markdown.MarkdownNodeRendererFactory;
+import org.commonmark.renderer.markdown.MarkdownRenderer;
 
 /**
  * Extension for adding task list items.
@@ -16,7 +22,8 @@ import org.commonmark.renderer.html.HtmlRenderer;
  *
  * @since 0.15.0
  */
-public class TaskListItemsExtension implements Parser.ParserExtension, HtmlRenderer.HtmlRendererExtension {
+public class TaskListItemsExtension implements Parser.ParserExtension, HtmlRenderer.HtmlRendererExtension,
+        MarkdownRenderer.MarkdownRendererExtension {
 
     private TaskListItemsExtension() {
     }
@@ -33,5 +40,20 @@ public class TaskListItemsExtension implements Parser.ParserExtension, HtmlRende
     @Override
     public void extend(HtmlRenderer.Builder rendererBuilder) {
         rendererBuilder.nodeRendererFactory(TaskListItemHtmlNodeRenderer::new);
+    }
+
+    @Override
+    public void extend(MarkdownRenderer.Builder rendererBuilder) {
+        rendererBuilder.nodeRendererFactory(new MarkdownNodeRendererFactory() {
+            @Override
+            public NodeRenderer create(MarkdownNodeRendererContext context) {
+                return new TaskListItemMarkdownNodeRenderer(context);
+            }
+
+            @Override
+            public Set<Character> getSpecialCharacters() {
+                return Set.of();
+            }
+        });
     }
 }
